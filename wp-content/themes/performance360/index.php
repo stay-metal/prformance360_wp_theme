5542a7f2ef125e0334c436e852261ea2702f758e
vadrums <vadlambrianov@gmail.com>
2020-05-04 21:09:18 +0300

    basic(front) :: add basic post meta basic functions

--- wp-content/themes/performance360/index.php
+++ wp-content/themes/performance360/index.php
@@ -3,9 +3,19 @@
     <?php if ( have_posts() ) { ?>
         <?php while ( have_posts() ) { ?>
             <?php the_post(); ?>
+             <div> 
+                <?php the_tags(); ?>
+            </div>
+            <div> 
+                <a href="<?php echo get_permalink() ?>">
+                    <?php $time_diff = human_time_diff( get_post_time('U'), current_time('timestamp') ); ?>
+                    <time><?php the_time(); ?></time>
+                </a>
+            </div>
             <h2>
-                <a href="<?php the_permalink(); ?> "><?php the_title(); ?></a>
+                <a href="<?php the_permalink(); ?>" title = "<?php the_title_attribute(); ?>" ><?php the_title(); ?></a>
             </h2>
+
         <?php } ?> 
     <?php } else { ?>
         <p>Записи не найдены</p>
