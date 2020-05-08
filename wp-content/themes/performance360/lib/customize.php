a96b8d2a98324ac1edc80f49c2e44f6428100179
vadlambrianov@gmail.com <Lambr21an0v>
2020-05-08 23:09:53 +0300

    feat(layout) : add two column layout in main loop

--- wp-content/themes/performance360/lib/customize.php
+++ wp-content/themes/performance360/lib/customize.php
@@ -1,7 +1,40 @@
 <?php
 
     function _themename_customize_register($wp_customize) {
+        // Footer section
+        $wp_customize->add_section('_themename_sidebar_options', array(
+            'title' => esc_html('Настройки Sidebar'),
+            'description' => 'Здесь можно изменить некоторые настройки sidebar',
+        ));
 
+        $theme_colors = array();
+        $theme_colors[] = array(
+            'slug'=>'color_section_name',
+            'default' => '#000000',
+            'label' => esc_html('Цвет фона боковой колонки')
+          );        
+
+        foreach( $theme_colors as $color ) {
+
+        }
+                
+        
+        // Footer section
+        $wp_customize->add_section('_themename_footer_options', array(
+            'title' => esc_html('Настройки Footer'),
+            'description' => 'Здесь можно изменить некоторые настройки секции footer',
+        ));
+        // Footer email field
+        $wp_customize->add_setting('_themename_info_email', array(
+            'default' => '',
+            'sanitize_callback' => 'sanitize_email'
+        ));
+        $wp_customize->add_control('_themename_info_email', array(
+            'type' => 'text',
+            'label' => esc_html('Email редакции'),
+            'section' => '_themename_footer_options'
+        ));
+        
     }
 
     add_action('customize_register', '_themename_customize_register');
--- wp-content/themes/performance360/template-parts/footer/info.php
+++ wp-content/themes/performance360/template-parts/footer/info.php
@@ -1,22 +1,28 @@
-<?php $column_layout = 12/( count(_themename_footer_widget_sidebars()) + 1); ?>
+<?php $column_layout = 12/( count(_themename_footer_widget_sidebars()) + 1); 
+$email = get_theme_mod( '_themename_info_email', '');
+?>
 <div class="o-row__column o-row__column--span-12 o-row__column--span-<?php echo $column_layout ?>@medium c-footer__info-container">
         <div class="o-row">
             <div class="o-row__column o-row__column--span-12">
             Logo (from customizer)
             </div>
+            <?php if( has_nav_menu('soc-menu') ){ ?>
             <div class="o-row__column o-row__column--span-12 u-flex">
             <span class="c-footer__soc">Читать в соцсетях: </span><?php get_template_part( 'template-parts/navigation/soc_navigation' ); ?>
-            </div>        
+            </div>   
+            <?php } ?>     
+            <?php if($email){ ?>
             <div class="o-row__column o-row__column--span-12">
                 <div class="o-row">
                     <div class="o-row__column o-row__column--span-12">   
                         <div class="c-footer__redation-title"> Редакция: </div>
                     </div>
                     <div class="o-row__column o-row__column--span-12">           
-                        <div class="c-footer__redation-email"> info@performance360.ru </div>
+                        <div class="c-footer__redation-email"> <?php echo esc_html($email);?> </div>
                     </div>
                 </div>  
             </div> 
+            <?php } ?>
         </div>
 
 </div>
