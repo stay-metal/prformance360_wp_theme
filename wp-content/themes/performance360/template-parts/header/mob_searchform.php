79524943a03de4f5c6d115636b43779d3f977759
obzenner <obzenner@Kinjas-MacBook-Pro.local>
2020-05-15 21:45:06 +0200

    feat(search) :: add mobile search func

--- wp-content/themes/performance360/template-parts/header/mob_searchform.php
+++ wp-content/themes/performance360/template-parts/header/mob_searchform.php
@@ -1,3 +1,12 @@
+<div class="o-container u-flex u-align-justify mobile-menu-top__search" >
+        <div class="mobile-search__trigger" data-open="false">
+            <i class="fas fa-search c-header__search-mobile-icon"></i>Поиск
+        </div>
+        <div class="mobile-search__form">
+            <?php get_search_form(); ?>
+        </div>
+    </div>
+<!-- 
 
-
-    <button class="c-header__search-mobile-button" type="submit"><i class="fas fa-search c-header__search-mobile-icon"></i> Поиск</button> 
+    <button class="c-header__search-mobile-button" type="submit">
+        <i class="fas fa-search c-header__search-mobile-icon"></i> Поиск</button>  -->
