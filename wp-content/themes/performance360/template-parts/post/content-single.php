e4c98d95fe97d5ecf2c5524c207980acb2f8d888
vadlambrianov@gmail.com <Lambr21an0v>
2020-05-16 02:46:51 +0300

    feat(teg_bg) starting with tag background db functions

--- wp-content/themes/performance360/template-parts/post/content-single.php
+++ wp-content/themes/performance360/template-parts/post/content-single.php
@@ -11,7 +11,9 @@
 
         <?php if ( get_the_post_thumbnail() !== '' ) { ?>
             <div class="c-post__thumbnail c-post-single__thumbnail">
-                <?php the_post_thumbnail( '_themename-single-thumb' ); ?>
+                <div class="c-post-single__thumbnail-border-radius">
+                    <?php the_post_thumbnail( '_themename-single-thumb' ); ?>
+                </div>
             </div>
         <?php } ?>
         <?php if (has_excerpt()) { ?>
@@ -25,17 +27,24 @@
         </div>
 
         <footer class="c-post__footer c-post-single__footer">
-            <?php 
-                if (has_category()) { 
-                    get_the_category_list( ', ');
-                } ?>
-            <div class="c-post__tags c-post-single__tags">
-            <?php 
-                if (has_tag()) { 
-                //    $tags_list = get_the_tag_list( '#',' #','');
-                   $tags_list = get_the_tag_list('<ul><li>','</li><li>','</li></ul>');
-                   echo $tags_list;
-                } ?>
+            <div class="o-row">
+                <div class="o-row__column o-row__column--span-9 o-row__column--span-9@medium o-row__column--span-9@mobile">
+                        <?php 
+                            if (has_category()) { 
+                                get_the_category_list( ', ');
+                            } ?>
+                        <div class="c-post__tags c-post-single__tags">
+                            <?php 
+                                if (has_tag()) { 
+                                //    $tags_list = get_the_tag_list( '#',' #','');
+                                //    $tags_list = get_the_tag_list('<ul><li>','</li><li>','</li></ul>');
+                                //    echo $tags_list;
+                                _themename_single_tags();
+                                } ?>
+                         </div>
+                    </div>
+                    <div class="o-row__column o-row__column--span-3 o-row__column--span-3@medium o-row__column--span-12@mobile">
+                    </div>
             </div>
         </footer>
 
