bee2f6212a66d901c6b4549c277726b6db575420
obzenner <obzenner@Kinjas-MacBook-Pro.local>
2020-05-15 19:58:12 +0200

    chore(mobile menu carousel) :: implementation

--- wp-content/themes/performance360/src/assets/js/components/owl.js
+++ wp-content/themes/performance360/src/assets/js/components/owl.js
@@ -1,1 +1,17 @@
 import 'owl.carousel';
+
+const mobileMenu = document.querySelector('.c-main-navigation--mobile');
+
+if (mobileMenu) {
+    const owlElement = mobileMenu.querySelector('.menu');
+    owlElement.classList.add('owl-carousel', 'owl-theme');
+    Array.from(owlElement.querySelectorAll('li')).forEach(el => {
+        const width = el.querySelector('a').offsetWidth;
+        el.style.width = width;
+    })
+    jQuery(owlElement).owlCarousel({
+        margin:10,
+        autoWidth:true,
+        items:4
+    });
+}
