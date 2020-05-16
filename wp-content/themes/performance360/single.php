1980e8f2449d69867598f6dda9f280fc424899a8
vadlambrianov@gmail.com <Lambr21an0v>
2020-05-16 23:01:56 +0300

    feat(func) :: add subscribe widhet to sidebar and footer

--- wp-content/themes/performance360/single.php
+++ wp-content/themes/performance360/single.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 <div class="o-container u-margin-top-30  u-margin-bottom-40 u-padding-right-0">
     <div class="o-row">
-         <div class="o-row__column o-row__column--span-12 o-row__column--span-12@small o-row__column--span-<?php echo is_active_sidebar('primary-sidebar') ? '8' : '12' ?>@mobile o-row__column--span-<?php echo is_active_sidebar('primary-sidebar') ? '8' : '12' ?>@medium u-padding-left-0 u-margin-left-0" >  
+    <div class="o-row__column o-row__column--span-12 o-row__column--span-12@small o-row__column--span-12@mobile o-row__column--span-<?php echo is_active_sidebar('primary-sidebar') ? '8' : '12' ?>@medium" > 
              <main role="main">
                 <?php get_template_part( 'loop', 'single'); ?>
             </main>
         </div>
-        <div class="o-row__column o-row__column--span-12 o-row__column--span-12@small o-row__column--span-<?php echo is_active_sidebar('primary-sidebar') ? '4' : '12' ?>@mobile o-row__column--span-4@medium">  
+        <?php if ( is_active_sidebar('primary-sidebar') ) { ?>
+        <div class="o-row__column o-row__column--span-12 o-row__column--span-12@small o-row__column--span-12@mobile o-row__column--span-4@medium">  
         <?php if ( is_active_sidebar('primary-sidebar') ) { ?>
             <?php get_sidebar(); ?>
-        <?php } ?>
+            <?php } ?>
         </div>
+        <?php } ?>
     </div>
 </div>
 <?php get_footer(); ?>
