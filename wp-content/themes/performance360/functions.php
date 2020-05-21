e551afc95ac47312edf4815c94b585b5e24d3704
obzenner <obzenner@Kinjas-MacBook-Pro.local>
2020-05-21 15:11:02 +0200

    rmv broken cond from _themename_loop_tags

--- wp-content/themes/performance360/functions.php
+++ wp-content/themes/performance360/functions.php
@@ -253,23 +253,21 @@ function _themename_loop_tags()
     $page = get_term_meta($tag->term_id, '_themename_page_field', true);
     $color = get_term_meta($tag->term_id, '_themename_tag_color', true);
     $bgColor = get_term_meta($tag->term_id, '_themename_tag_bg_color', true);
-    if (!empty($page)) {
-      $tag->page_rel = $page;
-      if (!empty($color)) {
-        $tag->color = $color;
-      } else {
-        $tag->color = 'AD6868';
-      }
-      if (!empty($bgColor)) {
-        $tag->bgColor = "#$bgColor";
-      } else {
-        $tag->bgColor = 'rgba(255, 0, 0, 0)';
-      }
-      $tags_final_array[] = $tag;
+    $tag->page_rel = $page;
+  
+    if (!empty($color)) {
+      $tag->color = $color;
+    } else {
+      $tag->color = 'AD6868';
     }
+    if (!empty($bgColor)) {
+      $tag->bgColor = "#$bgColor";
+    } else {
+      $tag->bgColor = 'rgba(255, 0, 0, 0)';
+    }
+    $tags_final_array[] = $tag;
   }
-  // var_dump( $tags_final_array);
-  // die;
+
   $tag_list = '';
   $i = 1;
   if (!empty($tags_final_array)) {
--- wp-content/themes/performance360/lib/helpers.php
+++ wp-content/themes/performance360/lib/helpers.php
@@ -1,11 +1,11 @@
 <?php
 
 function _themename_post_meta() { ?>
-    <?php $hasTags = _themename_single_tags() != ''; ?>
+    <?php $hasTags = _themename_loop_tags() != ''; ?>
     <div class="c-post__meta" >
         <?php if ($hasTags) { ?>
         <div class="c-post__tags u-align-middle">
-            <?php echo _themename_single_tags(); ?>
+            <?php echo _themename_loop_tags(); ?>
         </div>
         <?php } ?>
         <div class="c-post__time u-flex u-align-middle">
