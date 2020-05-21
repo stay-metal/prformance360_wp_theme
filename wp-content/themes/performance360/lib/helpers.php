e5fc01f0ee8b19bde7b0f71e8547a93905a85e2d
obzenner <obzenner@Kinjas-MacBook-Pro.local>
2020-05-21 11:42:00 +0200

    pull slider posts from category

--- wp-content/themes/performance360/lib/helpers.php
+++ wp-content/themes/performance360/lib/helpers.php
@@ -15,13 +15,13 @@ function _themename_post_meta() { ?>
 
 <?php
 function _themename_post_meta_for_slider() { ?>
-    <?php $tag = get_the_tags(); ?>
-    <?php $color = get_term_meta( $tag[0]->term_id, '_themename_tag_color', true ); ?>
+    <?php $tags = get_the_tags(); ?>
+    <?php $color = get_term_meta( $tags[0]->term_id, '_themename_tag_color', true); ?>
     <?php $color = ( ! empty( $color ) ) ? "#{$color}" : '#007DFF'; ?>
     <div class="o-row c-post__meta" >
         <div class="c-post__tags u-align-middle">
-            <a href="<?php echo esc_url( get_tag_link( $tag[0]->term_id ) ); ?>" class="c-post__tags-link" style="color: <?php echo $color; ?>">
-                <?php echo $tag[0]->name ?>
+            <a href="<?php echo esc_url( get_tag_link( $tags[0]->term_id ) ); ?>" class="c-post__tags-link" style="color: <?php echo $color; ?>">
+                <?php echo $tags[0]->name ?>
             </a>
         </div>
         <div class="c-post__time u-flex u-align-middle">
