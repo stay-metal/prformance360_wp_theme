be68ab895815f68d4b8240de8136826578196b9a
vadlambrianov@gmail.com <Lambr21an0v>
2020-05-24 19:03:12 +0300

    feat(style) add comments style

--- wp-content/themes/performance360/lib/comment-callback.php
+++ wp-content/themes/performance360/lib/comment-callback.php
@@ -1,18 +1,32 @@
 <?php
     function _themename_comment_callback ($comment, $args, $depth) {
         ?>
-        <li id="comment-<?php comment_ID(); ?>" <?php comment_class('c-comment', $comment->comment_parent ? 'c-comment_child' : '') ?> >
+
+        <li id="comment-<?php comment_ID(); ?>" <?php comment_class(['c-comment', $comment->comment_parent ? 'c-comment_child' : '']); ?> >
             <article class="c-comment-body"> 
-                <?php echo get_avatar($comment, 50, false, false,  array('class'=>'c-comments__avatar')); ?>
-            <div class="c-somments__content">
-                <div class="c-somments__author">
+                <?php echo get_avatar($comment, 50, false, false,  array('class'=>'c-comment__avatar')); ?>
+                <div class="c-comment__content">
+                    <div class="c-comment__meta">
+                        <div class="c-comment__author">
+                        <?php echo get_comment_author( $comment ); ?>
+                         </div>
+                        <div class="c-comment__time">
+                        <time datetime="<?php comment_time('c')?>">
+                            <?php echo human_time_diff(get_comment_time('U'), current_time('U')).' назад';?>
+                        <time>
+                        </div>
+                    </div>
+                        <?php //edit_comment_link('Редактировать', '<span class ="c-comment__edit-link">','</span>'); ?>
+                               
+                    <div class="c-comment__text">
+                        <?php comment_text();?>
+                    </div>
+                    <?php comment_reply_link(array(
+                        'depth' => $depth,
+                        'max_depth' => $args['max_depth']
+                     ));?>
                 </div>
-                <?php edit_comment_link('Редактировать', '<span class ="c-comments__edit-link">','</span>'); ?>
-                <time datetime="<?php comment_time('c')?>">
-                <?php echo human_time_diff(get_comment_time('U'), current_time('U')).' назад';?>
-                <time>
-            </div>
-            <article>
+             </article>
 
             </time></a>
         </li>
